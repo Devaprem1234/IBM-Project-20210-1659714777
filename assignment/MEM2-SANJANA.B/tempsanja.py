2b96dca66df664738bcf6fbf7e167cb1760a6d1a
Sowmiya1211 <113582351+Sowmiya1211@users.noreply.github.com>
2022-10-14 20:32:55 +0530

    Delete assignment directory

--- assignment/MEM2-SANJANA.B/tempsanja.py
+++
@@ -1,9 +0,0 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import random
-temp_sensor_value=random.randint(56,22)
-humidity_value=random.randint(34,87)
-if temp_sensor_value>30 or humidity_value>90:
-    print("Temperature value:",temp_sensor_value)
-    print("Humidity value:",humidity_value)
-    print("Hazard detected")
